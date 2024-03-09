8a57c7070c9c493a84a5b2393a3a6f318f34d42d
José Daniel Lara <larajosed@gmail.com>
2024-03-09 17:52:43 +0100

    user and role table creation script

---
+++ src/main/resources/db/changelog/changesets/01-users.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+--changeset larajosed:01 context:main
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  gender VARCHAR(45) NULL COMMENT '1 = men\n2 = woman\n3 = no binary',
+  adress VARCHAR(250) NULL,
+  role INT NULL COMMENT '1 = User\n2 = Admin',
+  date DATE NULL,
+  PRIMARY KEY (`id`));
+  
+CREATE TABLE roles (
+  id INT NOT NULL,
+  name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+  
+INSERT INTO roles (id, name) VALUES (1, 'Usuario');
+INSERT INTO roles (id, name) VALUES (2, 'Administrador');  
+  
