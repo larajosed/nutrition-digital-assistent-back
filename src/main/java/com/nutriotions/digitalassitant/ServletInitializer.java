76a32f8611a52a0a94ce247b6f42470fe8764751
José Daniel Lara <larajosed@gmail.com>
2024-03-09 14:14:49 +0100

    init: starting and configuring base project

---
+++ src/main/java/com/nutriotions/digitalassitant/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.nutriotions.digitalassitant;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootConfiguration
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NutritionsDigitalAssistantBackApplication.class);
+	}
+
+}
---
+++ src/test/java/com/nutriotions/digitalassitant/NutritionsDigitalAssistantBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutriotions.digitalassitant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionsDigitalAssistantBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
