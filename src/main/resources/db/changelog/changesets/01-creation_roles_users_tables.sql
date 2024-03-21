dc7478643d8343c47b45b3a0070ab4d7813f8e75
José Daniel Lara <larajosed@gmail.com>
2024-03-21 15:29:00 +0100

    creation of location and province tables and insertion of data

--- src/main/resources/db/changelog/changesets/01-creation_roles_users_tables.sql
+++ src/main/resources/db/changelog/changesets/01-creation_roles_users_tables.sql
@@ -1,22 +1,41 @@
 --liquibase formatted sql
 --changeset larajosed:01 context:main
 
+CREATE TABLE province (
+id INT NOT NULL,
+name VARCHAR(150) NOT NULL,
+
+PRIMARY KEY (id));
+
+CREATE TABLE location (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+id_province INT NOT NULL, 
+id_location INT NOT NULL,
+control_digit INT NOT NULL,
+name VARCHAR(150) NOT NULL,
+PRIMARY KEY (id));
+
 CREATE TABLE users (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  password VARCHAR(45) NOT NULL,
   gender INT NULL COMMENT '1 = Men\n2 = Woman\n3 = No binary',
+  birthdate DATE NULL,
   address VARCHAR(250) NULL,
+  postal_code INT NULL,
+  location VARCHAR (150) NULL,
+  province VARCHAR (150) NULL,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(45) NOT NULL,  
   role INT NULL COMMENT '1 = User\n2 = Admin',
-  date DATE NULL,
-  PRIMARY KEY (`id`));
+ 
+  PRIMARY KEY (id));
   
 CREATE TABLE roles (
   id INT NOT NULL,
   name VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (id));
   
 INSERT INTO roles (id, name) VALUES (1, 'Usuario');
 INSERT INTO roles (id, name) VALUES (2, 'Administrador');
+
--- src/main/resources/db/changelog/changesets/02-test_data_insertion.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
---changeset larajosed:01 context:test
-
-INSERT INTO users (id, name, last_name, email, password, gender, address, role, date) VALUES (1, 'david', 'mdl', 'david@gmail.com', '12345', 1, 'lacy', 1, '1990-10/14');
-INSERT INTO users (id, name, last_name, email, password, gender, address, role, date) VALUES (2, 'maria', 'mdl', 'maria@gmail.com', '12345', 1, 'lacy', 2, '1990-10/14');
